19fbefb0781f6ae3b7e76e04ee08f2989432c728
musafir928 <uganoghli@gmail.com>
2022-06-24 23:44:23 +0200

    add user crud

---
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.ResponseWrapper;
+import com.cydeo.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all-users")
+    public ResponseEntity<ResponseWrapper> getAllUsers() {
+        return ResponseEntity
+                .ok(
+                        new ResponseWrapper(
+                                "successfully got all users",
+                                userService.listAllUsers(),
+                                HttpStatus.OK
+                        )
+                );
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<ResponseWrapper> getUserByUsername(@PathVariable("username") String username) {
+        return ResponseEntity
+                .ok(
+                        new ResponseWrapper(
+                                "successfully got user",
+                                userService.findByUserName(username),
+                                HttpStatus.OK
+                        )
+                );
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO userDTO) {
+        userService.save(userDTO);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                        new ResponseWrapper(
+                                "successfully got user",
+                                HttpStatus.CREATED
+                        )
+                );
+    }
+
+}
