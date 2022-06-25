13a7ab0431400f950d42ae616fda7208f9248b8d
musafir928 <uganoghli@gmail.com>
2022-06-25 13:35:46 +0200

    add kc delete to userServiceImpl

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -94,6 +94,7 @@ public class UserServiceImpl implements UserService {
             user.setIsDeleted(true);
             user.setUserName(user.getUserName() + "-" + user.getId());
             userRepository.save(user);
+            keycloakService.delete(username);
         }
 
     }
